aba787559f3d79bdf818828e442e88fd1f15e2dd
dmitry.zarechnev@gmail.com <dmitry.zarechnev@gmail.com>
2022-11-15 18:44:36 +0300

    Change entities

--- Entities/ReceiptPosition.cs
+++ Entities/ReceiptPosition.cs
@@ -4,9 +4,11 @@ public class ReceiptPosition : Entity
 {
     public int ReceiptId { get; set; }
     public string? ProductName { get; set; }
-    public string? ProductCategory { get; set; }
+    public int? ProductCategoryId { get; set; }
+    public ProductCategory? ProductCategory { get; set; } 
     public double? Price { get; set; }
     public int Quantity { get; set; }
-    public string? UnitOfMeasurement { get; set; }
+    public int? UnitOfMeasurementId { get; set; }
+    public UnitOfMeasurement? UnitOfMeasurement { get; set; } 
     public double Coast { get; set; }
 }
